9dcbe512495f64d7dfa7f6b0a40d34a807143f3d
esquincaae <97558097+esquincaae@users.noreply.github.com>
2023-10-31 00:50:35 -0600

    problema de colisiones cuando un auto sale de su posicion

--- simulador.go
+++ simulador.go
@@ -4,107 +4,131 @@ import (
 	"math/rand"
 	"sync"
 	"time"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/faiface/pixel/imdraw"
 )
 
 const (
-	anchoVentana   = 1024.0
-	altoVentana    = 768.0
-	espacios       = 20
-	tamanoEspacio  = 40.0
-	altoEspacio    = 100.0
-	anchoAuto      = 30.0
-	altoAuto       = 60.0
-	velocidad      = 2.0
-	entradaY       = altoVentana / 2
+	anchoVentana    = 1024.0
+	altoVentana     = 768.0
+	espacios        = 20
+	tamanoEspacio   = 40.0
+	altoEspacio     = 100.0
+	anchoAuto       = 30.0
+	altoAuto        = 60.0
+	velocidad       = 2.0
+	entradaY        = altoVentana / 2
 	distanciaEntrada = 300.0
+	centroX         = anchoVentana / 2
+	centroY         = altoVentana / 2
 )
 
 type Auto struct {
-	posX, posY, dirX, metaY float64
+	posX, posY, dirX, dirY, metaX, metaY float64
+	estacionado                          bool
+	tiempoEstacionado                    time.Duration
 }
 
 type Estacionamiento struct {
-	espacios    [espacios]bool
-	enEspera    []*Auto
-	mu          sync.Mutex
+	espacios [espacios]bool
+	autos    []*Auto
+	mu       sync.Mutex
 }
 
 func NuevoEstacionamiento() *Estacionamiento {
 	return &Estacionamiento{}
 }
 
-func (e *Estacionamiento) estacionar() int {
+func (e *Estacionamiento) estacionar(auto *Auto) int {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
 	for i, ocupado := range e.espacios {
 		if !ocupado {
 			e.espacios[i] = true
+			auto.metaX = tamanoEspacio*float64(i%(espacios/2)) + tamanoEspacio/2 // X es el centro de la columna
+			fila := i / (espacios / 2)
+			if fila == 0 {
+				auto.metaY = altoVentana - altoEspacio/2 // Y es el centro para fila 0
+			} else {
+				auto.metaY = altoEspacio/2 // Y es el centro para fila 1
+			}
+			e.autos = append(e.autos, auto)
 			return i
 		}
 	}
 	return -1
 }
 
-func (e *Estacionamiento) salir(pos int) {
+func (e *Estacionamiento) salir(auto *Auto) {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
-	e.espacios[pos] = false
+	for i, a := range e.autos {
+		if a == auto {
+			e.espacios[i] = false
+			e.autos = append(e.autos[:i], e.autos[i+1:]...)
+			break
+		}
+	}
 }
 
 func (e *Estacionamiento) simularAuto(auto *Auto) {
-	// Anima el ingreso del auto al estacionamiento
-	for auto.posX < anchoVentana-distanciaEntrada {
+	auto.posX = 0
+	auto.posY = entradaY
+	auto.dirX = 1
+
+	for auto.posX < distanciaEntrada {
 		auto.posX += velocidad
 		time.Sleep(time.Millisecond * 10)
 	}
-	auto.dirX = 1
-	auto.metaY = altoVentana - altoAuto/2
+	auto.dirX = 0
 
-	pos := e.estacionar()
-	if pos != -1 {
-		targetX := tamanoEspacio*float64(pos%(espacios/2)) + tamanoEspacio/2
-		if pos >= espacios/2 {
-			auto.metaY = altoAuto/2
-		}
-
-		for auto.posX != targetX || auto.posY != auto.metaY {
-			if auto.posX < targetX {
-				auto.posX += velocidad
-			} else if auto.posX > targetX {
-				auto.posX -= velocidad
+	for {
+		auto.estacionado = false
+		time.Sleep(time.Second * time.Duration(rand.Intn(10)+1)) // Esperar un momento antes de intentar estacionarse
+		pos := e.estacionar(auto)
+		if pos != -1 {
+			// Mover al auto al centro de la pantalla
+			for auto.posX != auto.metaX || auto.posY != auto.metaY {
+				if auto.posX < auto.metaX {
+					auto.posX += velocidad
+				} else if auto.posX > auto.metaX {
+					auto.posX -= velocidad
+				}
+				if auto.posY < auto.metaY {
+					auto.posY += velocidad
+				} else if auto.posY > auto.metaY {
+					auto.posY -= velocidad
+				}
+				time.Sleep(time.Millisecond * 10)
 			}
-			if auto.posY < auto.metaY {
-				auto.posY += velocidad
-			} else if auto.posY > auto.metaY {
-				auto.posY -= velocidad
+
+			auto.estacionado = true
+
+			auto.tiempoEstacionado = time.Second * time.Duration(rand.Intn(50)+10)
+			time.Sleep(auto.tiempoEstacionado)
+
+			// Mover al auto a una posición vacía
+			auto.dirX = -velocidad
+			for auto.posX > 0 {
+				auto.posX += auto.dirX
+				time.Sleep(time.Millisecond * 10)
 			}
-			time.Sleep(time.Millisecond * 10)
+			e.salir(auto)
+		} else {
+			// Si no hay espacio, el auto se va
+			for auto.posX > 0 {
+				auto.posX += auto.dirX
+				time.Sleep(time.Millisecond * 10)
+			}
 		}
-		auto.dirX = 0
-
-		// Espera un tiempo aleatorio
-		duracion := time.Second * time.Duration(rand.Intn(50)+10)
-		time.Sleep(duracion)
-		e.salir(pos)
-
-		// Anima la salida del auto
-		for auto.posX > 0 {
-			auto.posX -= velocidad
-			time.Sleep(time.Millisecond * 10)
-		}
-	} else {
-		// Si no hay espacio, el auto se va
-		for auto.posX > 0 {
-			auto.posX -= velocidad
-			time.Sleep(time.Millisecond * 10)
-		}
+		time.Sleep(time.Second * time.Duration(rand.Intn(100)+10))
 	}
 }
+
 
 func run() {
 	cfg := pixelgl.WindowConfig{
@@ -119,18 +143,13 @@ func run() {
 
 	e := NuevoEstacionamiento()
 
+	imd := imdraw.New(nil) // Crear una sola instancia de imdraw.IMDraw
+
 	go func() {
 		for {
-			auto := &Auto{
-				posX: 0,
-				posY: entradaY,
-				dirX: 1,
-			}
-			e.mu.Lock()
-			e.enEspera = append(e.enEspera, auto)
-			e.mu.Unlock()
+			auto := &Auto{}
 			go e.simularAuto(auto)
-			time.Sleep(time.Second * 2)
+			time.Sleep(time.Second * time.Duration(rand.Intn(10)+1))
 		}
 	}()
 
@@ -138,37 +157,38 @@ func run() {
 		win.Clear(pixel.RGB(0.9, 0.9, 0.9))
 
 		// Dibujar estacionamiento
-		imd := imdraw.New(nil)
+		imd.Clear() // Limpiar la instancia imd antes de dibujar las divisiones
 		imd.Color = pixel.RGB(0.5, 0.5, 0.5)
 		for i := 0; i < espacios/2; i++ {
 			// Parte superior
-			imd.Push(pixel.V(tamanoEspacio*float64(i), altoVentana-altoEspacio))
-			imd.Push(pixel.V(tamanoEspacio*float64(i+1), altoVentana))
+			imd.Push(pixel.V(tamanoEspacio*float64(i)+tamanoEspacio/2, altoVentana-altoEspacio/2))
+			imd.Push(pixel.V(tamanoEspacio*float64(i+1)+tamanoEspacio/2, altoVentana))
 			imd.Rectangle(0)
 
 			// Parte inferior
-			imd.Push(pixel.V(tamanoEspacio*float64(i), 0))
-			imd.Push(pixel.V(tamanoEspacio*float64(i+1), altoEspacio))
+			imd.Push(pixel.V(tamanoEspacio*float64(i)+tamanoEspacio/2, altoEspacio/2))
+			imd.Push(pixel.V(tamanoEspacio*float64(i+1)+tamanoEspacio/2, 0))
 			imd.Rectangle(0)
 		}
 		imd.Draw(win)
 
 		// Dibujar autos
 		e.mu.Lock()
-		for _, auto := range e.enEspera {
-			imd := imdraw.New(nil)
+		for _, auto := range e.autos {
 			imd.Color = pixel.RGB(0, 0, 1)
-			imd.Push(pixel.V(auto.posX, auto.posY))
-			imd.Push(pixel.V(auto.posX+anchoAuto, auto.posY+altoAuto))
+			imd.Push(pixel.V(auto.posX-anchoAuto/2, auto.posY-altoAuto/2))
+			imd.Push(pixel.V(auto.posX+anchoAuto/2, auto.posY+altoAuto/2))
 			imd.Rectangle(0)
-			imd.Draw(win)
 		}
 		e.mu.Unlock()
+
+		imd.Draw(win)
 
 		win.Update()
 	}
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	pixelgl.Run(run)
 }
